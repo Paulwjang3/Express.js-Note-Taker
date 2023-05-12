60589c8102275c219169eb56850d25936d8fc3f5
Paulwjang3 <128998742+Paulwjang3@users.noreply.github.com>
2023-05-12 05:54:25 -0400

    updated url

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { isUtf8 } = require('buffer');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -27,7 +28,7 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes/', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
@@ -40,6 +41,31 @@ app.post('/api/notes', (req, res) => {
   }); 
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+  let deleteNote = JSON.parse(req.params.id);
+  console.log("Note to be deleted: " ,deleteNote);
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+    if (error) {
+        return console.log(error)
+    }
+   let notesArray = JSON.parse(notes);
+   //loop through notes array and remove note with id matching deleteId
+   for (var i=0; i<notesArray.length; i++){
+     if(deleteNote == notesArray[i].id) {
+       notesArray.splice(i,1);
+
+       fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notesArray), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(notesArray)
+        res.json(notesArray);
+      })
+     }
+  }
+  
+}); 
+});
 
 app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`)
